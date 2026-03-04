3cbeada8a52ad027df21e47ea74bafe678decfca
Yejin Oh <131301186+MewMewPewPew@users.noreply.github.com>
2026-03-04 12:42:24 -0500

    added stuff a button and js

--- js/project1.js
+++ js/project1.js
@@ -1,17 +1,20 @@
 window.onload = function (){
-//div documents
-let mindBox = this.document.getElementById("mindBoxOnly");
-
-    //button on&off interactivity
-    let wakeUpButton = document.getElementById("awakeButton");
     let sleep = false;
-    wakeUpButton.addEventListener("click", chiikaAwake);
-    //img of the tamagotchi
+    let egg = true; 
+    let eggCracked = false;
+    let hatched = false;
+    //div documents
+    let mindBox = this.document.getElementById("mindBoxOnly");
+    //img of the tamagotchi (machine)
     let tamagochiImg = document.getElementById("tamagochiOff");
     //virtual pet Id
     let virtualPetDoc = this.document.getElementById("pet");
-    let egg = true; 
-    let eggCracked = false;
+
+    //buttons
+    let wakeUpButton = document.getElementById("awakeButton");
+    let actButton = document.getElementById("interactButton");
+
+    wakeUpButton.addEventListener("click", chiikaAwake);
     function chiikaAwake(){
         if (!sleep){
             //console.log("chiika is awake");
@@ -24,8 +27,9 @@ let mindBox = this.document.getElementById("mindBoxOnly");
             document.body.style.background = "rgb(253, 181, 204)";
             // mindBox.style.display = "none";
 
-            //Crack the egg, yay pet
-            virtualPet();
+            //make the buttons accessable
+            interactPet();
+            actButton.src = "assets/img/button-base.png";
         }
         else if(sleep){
             //console.log("chiika is asleep");
@@ -34,6 +38,8 @@ let mindBox = this.document.getElementById("mindBoxOnly");
             wakeUpButton.src = "assets/img/buttonPink-off.png";
             tamagochiImg.src = "assets/img/Tamagochi_off.png";
             virtualPetDoc.style.display ="none";
+
+
             //Resetting the virtual pet
             virtualPetDoc.src = "assets/gif/eggIMG.png";
             egg = true;
@@ -44,36 +50,84 @@ let mindBox = this.document.getElementById("mindBoxOnly");
             
         } 
     }
-    function virtualPet(){
-        //will have maybe more complicated state of the pet
+    function interactPet(){
+        //button interact with pet for now 
         if(egg){
-            if(!eggCracked){
+            console.log(egg);
+            if (!hatched){
                 //makes the egg move when mouse over
-                virtualPetDoc.addEventListener("mouseover", function(e){
+                actButton.addEventListener("click", breakingShell())
+                {
                     console.log("What could be in the egg?");
-                    this.src = "assets/gif/egg.gif";
-                })
+                    virtualPetDoc.src = "assets/gif/egg.gif";
+                    // eggCracked = true;
+                    
+                };
                 virtualPetDoc.addEventListener("mouseout", function(e){
                     this.src = "assets/gif/eggIMG.png";
-                })
-            } 
+                    console.log("mouseOut");
+                    // eggCracked = true;
+                });
+            }
+                
             //makes the egg hatch
             virtualPetDoc.addEventListener("click", function(e){ // has to be done only once
                 console.log("New kitty has been born");
                 this.src = "assets/gif/babyKittyegg.gif";
                 egg = false;
-                eggCracked = true;
+                if (!egg){
+                    console.log(egg + "2");
+                    return;   
+                } 
             })
-            // else if (!egg){
-            // }
         }
-        else if (eggCracked){
-            
+        else if (!egg){
             virtualPetDoc.addEventListener("click", function(e){
-                console.log("Hello Kitty baby");
-                this.src = "assets/gif/babyKitty.gif";
+            this.src = "assets/gif/babyKitty.gif";
             })
+            
         }
+
+        // if(egg){
+        //     if (!hatched) {
+        //         //makes the egg move when mouse over
+        //         virtualPetDoc.addEventListener("mouseover", function(e){
+        //             console.log("What could be in the egg?");
+        //             this.src = "assets/gif/egg.gif";
+        //             // eggCracked = true;
+        //             if (hatched) {
+        //                 virtualPetDoc.addEventListener("mouseover", function(e){
+        //                 this.src = "assets/gif/babyKitty.gif";
+        //                 })
+        //             }
+        //         });
+        //         virtualPetDoc.addEventListener("mouseout", function(e){
+        //             this.src = "assets/gif/eggIMG.png";
+        //             console.log("mouseOut");
+        //             // eggCracked = true;
+        //             if (hatched) {
+        //                 virtualPetDoc.addEventListener("mouseout", function(e){
+        //                 this.src = this.src;
+        //                 })
+        //             }
+        //         });
+        //         //makes the egg hatch
+        //         virtualPetDoc.addEventListener("click", function(e){ // has to be done only once
+        //             console.log("New kitty has been born");
+        //             this.src = "assets/gif/babyKittyegg.gif";
+        //             // egg = false;
+        //             hatched = true;
+        //              if (hatched) {
+        //                 virtualPetDoc.addEventListener("mouseout", function(e){
+        //                 this.src = "assets/gif/babyKitty.gif";
+        //                 })
+        //             }
+        //         });
+
+        //         }
+        //     // else if (!egg){
+        //     // }
+        // }
         
 
     }
