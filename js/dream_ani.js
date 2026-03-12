d12cc7dbea8cbc81efcededf083aa6f430a980ae
MewMewPewPew <toleary.oest@gmail.com>
2026-03-12 00:48:59 -0400

    Animation
    dream animation :3

--- js/dream_ani.js
+++ js/dream_ani.js
@@ -1,8 +1,13 @@
 window.onload = function () {
     // get the canvas
-    let canvas = document.getElementById("testCanvas");
+    let canvas = document.getElementById("dreamCanvas");
     //get the context
     let context = canvas.getContext("2d");
-
-
+    
+    let rectSize = 50
+  //a draw a rect:
+  context.fillStyle = "#000000";
+  // draw a rect
+  context.fillRect(canvas.width / 2, canvas.height / 2, rectSize, rectSize);
+ 
 }
--- js/project1.js
+++ js/project1.js
@@ -4,35 +4,181 @@
 
 window.onload = function (){
     let sleep = false;
+    let dreaming = true;
     let clickAct = 0;
-    //div documents
-    let mindBox = this.document.getElementById("mindBoxOnly");
-    //img of the tamagotchi (machine)
+    //img of the tamagotchi 
     let tamagochiImg = document.getElementById("tamagochiOff");
+    let tamagotchi = document.getElementById("chiikaBoxOnly");
     //virtual pet ID
-    let virtualPetDoc = this.document.getElementById("pet"); 
+    let virtualPetDoc = document.getElementById("pet"); 
     let questionNameBox = document.getElementById("inputBoxOnly");
     let vPetNameValue;  
     let vPetName = document.getElementById("vPetNameDisplay");
     let nameList = [];
     let named = false;
-    // let storageVPetName;
     //buttons
-    //check petRef.png to know which number refers to which button
+        //check petRef.png to know which number refers to which button
     let wakeUpButton = document.getElementById("awakeButton");
     let actButton = document.getElementById("interactButton");
     let actButtonHappy = document.getElementById("interactButton2");
     let actButtonSad = document.getElementById("interactButton3");
     let actButtonPlay = document.getElementById("interactButton4");
     let actButtonEat = document.getElementById("interactButton5");
+    
+    //Virtual pet dream/mind Box animation
+    let mindBox = this.document.getElementById("mindBoxOnly");
+    let canvas = document.getElementById("dreamCanvas");
+    let context = canvas.getContext("2d");
+    let startAngle = 0;
+    let endAngle = Math.PI * 2; //full rotation
+    let r1 = 50;
+    let counter = 0;
+    let counterNum = 0.005;
+    
+    if (!sleep) {
+        dreamAnimation();
+        
+        // else {
+            //             cancelAnimationFrame(animateFlowerDream) ;
+            //         }
+            
+        } 
+        function dreamAnimation (){
+        // if (clickAct<= 1) {
+            tamagotchi.addEventListener("mouseenter",function(e){
+                
+                console.log(counterNum);
+                // if (clickAct >= 1) {
+                //     counterNum = 0.06;
+                // }else if (sleep) {
+                //     counterNum = 0.04;
+                // }  
+                if (dreaming) {
+                    counterNum += 0.03;
+                }
+                else if (clickAct === 1){
+                counterNum = 0.07;
+                }
+                else if(clickAct === 2){
+                    counterNum = 0;
+                }
+                else {
+                    counterNum = 0.05;
+                }
+                
+            })
+            tamagotchi.addEventListener("mouseleave",function(e){
+                
+                // console.log(counterNum);
+                // if (clickAct >=1) {
+                //     counterNum = 0.06;
+                // } else if (sleep) {
+                //     counterNum = 0.04;
+                // } 
+                if (dreaming) {
+                    counterNum -= 0.03;
+                }
+                else if (clickAct === 1){
+                counterNum = 0.07;
+                }
+                else if(clickAct === 2){
+                    counterNum = 0;
+                }
+                else {
+                    counterNum = 0.05;
+                }
+                
+            })
+            requestAnimationFrame(animateFlowerDream);
+        // } 
+        console.log("dreaming...");
+        
+        // context.fillRect(canvas.width / 2, canvas.height / 2, 20, 20);
+        canvas.style.background = "#121f1bb7";
+        createFlower(canvas.width / 2,canvas.height / 2, 30,["rgb(241, 148, 195)","rgba(241, 148, 194, 0.45)", "#86aca013", "#00000001"], ["#86aca095", "#86aca095","rgb(0, 0, 0)", "rgb(253, 181, 204)","rgba(241, 148, 194, 0.69)"])
+        
+    }
+    function createFlower(xPos,yPos,radius,colors, colorS){
+        //bottom +
+        drawSinglePetal(xPos,yPos+3*radius/2,radius,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos+3*radius/2 +5,radius/1.2,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos+3*radius/2 +10,radius/1.5,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos+3*radius/2 +15,radius/2,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos+3*radius/2 +20,radius/3,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos+3*radius/2 +25,radius/6,colors[2], colorS[0])
+        //top
+        drawSinglePetal(xPos,yPos-3*radius/2,radius,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos-3*radius/2 -5,radius/1.2,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos-3*radius/2 -10,radius/1.5,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos-3*radius/2 -15,radius/2,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos-3*radius/2 -20,radius/3,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos-3*radius/2 -25,radius/6,colors[2], colorS[0])
+        //left
+        drawSinglePetal(xPos-3*radius/2,yPos,radius,colors[2], colorS[0])
+        drawSinglePetal(xPos-3*radius/2 -5,yPos,radius/1.2,colors[2], colorS[0])
+        drawSinglePetal(xPos-3*radius/2 -10,yPos,radius/1.5,colors[2], colorS[0])
+        drawSinglePetal(xPos-3*radius/2 -15,yPos,radius/2,colors[2], colorS[0])
+        drawSinglePetal(xPos-3*radius/2 -20,yPos,radius/3,colors[2], colorS[0])
+        drawSinglePetal(xPos-3*radius/2 -25,yPos,radius/6,colors[2], colorS[0])
+        //right
+        drawSinglePetal(xPos+3*radius/2,yPos,radius,colors[2], colorS[0])
+        drawSinglePetal(xPos+3*radius/2 +5,yPos,radius/1.2,colors[2], colorS[0])
+        drawSinglePetal(xPos+3*radius/2 +10,yPos,radius/1.5,colors[2], colorS[0])
+        drawSinglePetal(xPos+3*radius/2 +15,yPos,radius/2,colors[2], colorS[0])
+        drawSinglePetal(xPos+3*radius/2 +20,yPos,radius/3,colors[2], colorS[0])
+        drawSinglePetal(xPos+3*radius/2 +25,yPos,radius/6,colors[2], colorS[0])
+        //middle
+        drawSinglePetal(xPos,yPos,radius,colors[3], colorS[1])
+        drawSinglePetal(xPos,yPos,radius/1.2,colors[3], colorS[1])
+        drawSinglePetal(xPos,yPos,radius/1.5,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos,radius/2,colors[2], colorS[0])
+        drawSinglePetal(xPos,yPos,radius/3,colors[1], colorS[1])
+        drawSinglePetal(xPos,yPos,radius/6,colors[0], colorS[1])
 
+    }
+    function drawSinglePetal(x,y,r,fillColor, strokeColor){
+        context.fillStyle = fillColor; // change the color we are using
+        context.strokeStyle = strokeColor; // change the color we are using
+        context.beginPath();
+        context.arc(x,y,r,startAngle,endAngle, true);
+        context.fill(); // set the fill
+        context.lineWidth=2; //change stroke
+        context.stroke();//set the stroke
+        context.closePath();
+    }
+    // requestAnimationFrame(animateFlowerDream);
+    function animateFlowerDream(){
+        counter += counterNum;
+        //repaint with a black rect... like the background in draw in p5.js
+        context.clearRect(0,0,canvas.width,canvas.height);
+        //console.log("go")
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        //we will make a scaling animation:
+        //math.sin goes from [-1,1]*30 == -30,30
+        //then if we add 30 to the min ==0 and to the max will be 60 :)
+        let newSize = Math.sin(counter) * r1 + r1;
+        let newSize2 = Math.sin(counter*2) * r1 + r1;
+        createFlower(canvas.width / 2, canvas.height / 2, newSize, ["rgb(241, 148, 195)","rgba(241, 148, 194, 0.45)", "#86aca063", "#00000001"], ["#86aca095", "#86aca095","rgb(0, 0, 0)", "rgb(253, 181, 204)","rgba(241, 148, 194, 0.69)"]);
+        //another flower 
+        createFlower(canvas.width / 2, canvas.height / 4, newSize2/2, ["rgb(241, 148, 195)","rgba(241, 148, 194, 0.45)", "#86aca063", "#00000001"], ["#86aca095", "#86aca095","rgb(0, 0, 0)", "rgb(253, 181, 204)","rgba(241, 148, 194, 0.69)"]);
+        //having fun
+        createFlower(canvas.width / 2, canvas.height / 1.35, newSize2/2, ["rgb(241, 148, 195)","rgba(241, 148, 194, 0.45)", "#86aca063", "#00000001"], ["#86aca095", "#86aca095","rgb(0, 0, 0)", "rgb(253, 181, 204)","rgba(241, 148, 194, 0.69)"]);
+        createFlower(canvas.width / 4, canvas.height / 2, newSize2/2, ["rgb(241, 148, 195)","rgba(241, 148, 194, 0.45)", "#86aca063", "#00000001"], ["#86aca095", "#86aca095","rgb(0, 0, 0)", "rgb(253, 181, 204)","rgba(241, 148, 194, 0.69)"]);
+        createFlower(canvas.width / 1.35, canvas.height / 2, newSize2/2, ["rgb(241, 148, 195)","rgba(241, 148, 194, 0.45)", "#86aca063", "#00000001"], ["#86aca095", "#86aca095","rgb(0, 0, 0)", "rgb(253, 181, 204)","rgba(241, 148, 194, 0.69)"]);
+        requestAnimationFrame(animateFlowerDream);
+    }
+ 
+
+    //Event when power button is clicked
     wakeUpButton.addEventListener("click", chiikaAwake);
     function chiikaAwake(){
         if (!sleep){
             //console.log("chiika is awake");
             sleep = true;
+            dreaming = false;
             //Awaken mode
             wakeUpButton.src = "assets/img/buttonPink-on.png";
+            
             tamagochiImg.src = "assets/img/Tamagochi_on.png";
             
             virtualPetDoc.style.display ="block";
@@ -49,18 +195,16 @@ window.onload = function (){
         else if(sleep){
             //console.log("chiika is asleep");
             sleep = false;
+            dreaming = true;
             //Asleep mode
             wakeUpButton.src = "assets/img/buttonPink-off.png";
             tamagochiImg.src = "assets/img/Tamagochi_off.png";
             virtualPetDoc.style.display ="none";
 
-
             //Resetting the virtual pet
             virtualPetDoc.src = "assets/gif/eggIMG.png";
             vPetName.style.display = "none";
             named = false;
-            // egg = true;
-            // eggCracked = false;
             //Resetting the console/pet 
             vPetNameValue = "NoName";
             clickAct = 0;
@@ -80,15 +224,17 @@ window.onload = function (){
         wakeUpButton.addEventListener("mouseup", function(e){
             this.src = "assets/img/buttonPink-on.png";
         }) 
+        canvas.style.background = "#f6b9fc";
+        
     }
     
     function interactPet(){
         //button interact with pet for now 
-        // if (!sleep) {
-            // }
+       
         actButton.src = "assets/img/button-base.png";
             // clicks to crack the egg
         actButton.addEventListener("click", function (e){
+            
             virtualPetDoc.src = "assets/gif/egg.gif";
             if (sleep){
                 clickAct ++;
@@ -96,7 +242,11 @@ window.onload = function (){
                 
             }
             console.log(clickAct);
-            if(clickAct === 2){
+            if (clickAct === 1){
+                counterNum = 0.07;
+            }
+            else if(clickAct === 2){
+                counterNum = 0;
                 virtualPetDoc.src = "assets/gif/babyKittyegg.gif";
             }
             else if (clickAct >= 3){
@@ -112,6 +262,7 @@ window.onload = function (){
                 actButtonEat.src = "assets/img/button-base.png";
             }
         })
+        // change the gif source when button is clicked
         actButtonHappy.addEventListener("click", function (e){
             console.log("b2");
             if (clickAct >= 3){
@@ -222,7 +373,7 @@ window.onload = function (){
                     // const str = localStorage.getItem("storageVPetName");
                     // // convert string to valid object
                     // const parsedVPNames = JSON.parse(str);
-                    
+
                     // console.log(parsedVPNames);
                     // localStorage.setItem(parentID,storageVPetName);
                 } 
